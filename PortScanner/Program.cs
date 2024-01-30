7971b21e574cf343868d7c29ea4e9a6320871580
Dan Barton <danbarton2000@gmail.com>
2024-01-30 22:06:39 +0000

    Using TCP to scan ports.
    Added validation to ports. Prints to console whenever something important happens.

--- PortScanner/Program.cs
+++ PortScanner/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.CommandLine;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 var rootCommand = new RootCommand("PortScanner");
 var address = new Argument<string>(
@@ -17,9 +18,7 @@ await rootCommand.InvokeAsync(args);
 
 static async Task Handler(string address, string includePorts)
 {
-	Console.WriteLine($"{address} - {includePorts}");
-
-	if (IPAddress.TryParse(address, out IPAddress? ip))
+	if (IPAddress.TryParse(address, out IPAddress ip))
 	{
 		Console.WriteLine("Valid IP address!");
 	}
@@ -28,6 +27,7 @@ static async Task Handler(string address, string includePorts)
 		try
 		{
 			var ipHostInfo = await Dns.GetHostEntryAsync(address);
+			ip = ipHostInfo.AddressList[1];
 		}
 		catch (ArgumentException)
 		{
@@ -39,10 +39,61 @@ static async Task Handler(string address, string includePorts)
 			Console.WriteLine($"Could not resolve {address}");
 			return;
 		}
-		catch (Exception ex) 
+		catch (Exception ex)
 		{
-			Console.WriteLine($"An unknown error as occurred: {ex}");
+			Console.WriteLine($"An unknown error has occurred: {ex}");
 			return;
 		}
 	}
+
+	Console.WriteLine($"IP address: {ip}");
+
+	var split = includePorts.Split('-');
+	List<int> ports = new();
+
+	if (split.Length == 1)
+	{
+		if (!int.TryParse(split[0], out int port))
+		{
+			Console.WriteLine($"Could not parse port {port}");
+			return;
+		}
+		ports.Add(port);
+	}
+	else if (split.Length == 2)
+	{
+		if (!int.TryParse(split[0], out int port1))
+		{
+			Console.WriteLine($"Could not parse port {port1}");
+			return;
+		}
+		if (!int.TryParse(split[1], out int port2))
+		{
+			Console.WriteLine($"Could not parse port {port2}");
+			return;
+		}
+		ports.AddRange(Enumerable.Range(port1, port2 - port1 + 1));
+	}
+	else
+	{
+		Console.WriteLine($"Too many arguments: {includePorts}");
+		return;
+	}
+
+	foreach (var port in ports) 
+	{
+		using TcpClient tcpClient = new();
+		try
+		{
+			tcpClient.Connect(ip.ToString(), port);
+			Console.WriteLine($"Connected to port {port}");
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine(ex.Message);
+		}
+	}
+
+	Console.WriteLine("Finished scan");
+	Console.ReadLine();
 }
