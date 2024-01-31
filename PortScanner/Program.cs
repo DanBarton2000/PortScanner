0e231dd24be58fb61d4af22dcbc01f1c9fd531c8
Dan Barton <danbarton2000@gmail.com>
2024-01-31 22:23:29 +0000

    Refactored into TcpScanner
    Now prints out the number of open and closed ports.

--- PortScanner/Program.cs
+++ PortScanner/Program.cs
@@ -1,7 +1,7 @@
-﻿using System.CommandLine;
+﻿using PortScanner;
+using System.CommandLine;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 
 var rootCommand = new RootCommand("PortScanner");
 var address = new Argument<string>(
@@ -18,82 +18,18 @@ await rootCommand.InvokeAsync(args);
 
 static async Task Handler(string address, string includePorts)
 {
-	if (IPAddress.TryParse(address, out IPAddress ip))
+	TcpScanner tcpScanner = new();
+
+	try
 	{
-		Console.WriteLine("Valid IP address!");
+		await tcpScanner.SetAddress(address);
+		tcpScanner.SetIncludePorts(includePorts);
+		tcpScanner.Scan();
 	}
-	else
+	catch (Exception ex)
 	{
-		try
-		{
-			var ipHostInfo = await Dns.GetHostEntryAsync(address);
-			ip = ipHostInfo.AddressList[1];
-		}
-		catch (ArgumentException)
-		{
-			Console.WriteLine($"{address} is an invalid IP address.");
-			return;
-		}
-		catch (SocketException)
-		{
-			Console.WriteLine($"Could not resolve {address}");
-			return;
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine($"An unknown error has occurred: {ex}");
-			return;
-		}
+		Console.WriteLine(ex.Message);
 	}
 
-	Console.WriteLine($"IP address: {ip}");
-
-	var split = includePorts.Split('-');
-	List<int> ports = new();
-
-	if (split.Length == 1)
-	{
-		if (!int.TryParse(split[0], out int port))
-		{
-			Console.WriteLine($"Could not parse port {port}");
-			return;
-		}
-		ports.Add(port);
-	}
-	else if (split.Length == 2)
-	{
-		if (!int.TryParse(split[0], out int port1))
-		{
-			Console.WriteLine($"Could not parse port {port1}");
-			return;
-		}
-		if (!int.TryParse(split[1], out int port2))
-		{
-			Console.WriteLine($"Could not parse port {port2}");
-			return;
-		}
-		ports.AddRange(Enumerable.Range(port1, port2 - port1 + 1));
-	}
-	else
-	{
-		Console.WriteLine($"Too many arguments: {includePorts}");
-		return;
-	}
-
-	foreach (var port in ports) 
-	{
-		using TcpClient tcpClient = new();
-		try
-		{
-			tcpClient.Connect(ip.ToString(), port);
-			Console.WriteLine($"Connected to port {port}");
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine(ex.Message);
-		}
-	}
-
-	Console.WriteLine("Finished scan");
 	Console.ReadLine();
 }
---
+++ PortScanner/TcpScanner.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortScanner
+{
+	internal class TcpScanner
+	{
+        private IPAddress _address = IPAddress.Loopback;
+        private readonly List<int> _ports = new();
+
+		private int _openPorts = 0;
+		private int _closedPorts = 0;
+
+		public void Scan()
+		{
+			Console.WriteLine("Starting scan");
+
+			foreach (var port in _ports)
+			{
+				using TcpClient tcpClient = new();
+				try
+				{
+					tcpClient.Connect(_address.ToString(), port);
+					Console.WriteLine($"Connected to port {port}");
+					_openPorts++;
+				}
+				catch (SocketException ex)
+				{
+					Console.WriteLine($"{ex.Message} {ex.ErrorCode}");
+					_closedPorts++;
+				}
+			}
+
+			Console.WriteLine("Finished scan");
+			Console.WriteLine($"Open ports: {_openPorts}");
+			Console.WriteLine($"Closed ports: {_closedPorts}");
+		}
+
+        public async Task SetAddress(string address)
+        {
+			if (IPAddress.TryParse(address, out IPAddress? ip))
+			{
+				Console.WriteLine("Valid IP address!");
+				_address = ip;
+			}
+			else
+			{
+				try
+				{
+					var ipHostInfo = await Dns.GetHostEntryAsync(address);
+					_address = ipHostInfo.AddressList[1];
+				}
+				catch
+				{
+					throw;
+				}
+			}
+
+			Console.WriteLine($"IP address: {_address}");
+		}
+
+		public void SetIncludePorts(string ports)
+		{
+			var split = ports.Split('-');
+
+			if (split.Length == 1)
+			{
+				if (!int.TryParse(split[0], out int port))
+				{
+					throw new ArgumentException($"Could not parse port '{split[0]}'");
+				}
+				_ports.Add(port);
+			}
+			else if (split.Length == 2)
+			{
+				if (!int.TryParse(split[0], out int port1))
+				{
+					throw new ArgumentException($"Could not parse port '{split[0]}'");
+				}
+				if (!int.TryParse(split[1], out int port2))
+				{
+					throw new ArgumentException($"Could not parse port '{split[1]}'");
+				}
+				_ports.AddRange(Enumerable.Range(port1, port2 - port1 + 1));
+			}
+			else
+			{
+				throw new ArgumentException($"Too many arguments: '{ports}'");
+			}
+		}
+    }
+}
